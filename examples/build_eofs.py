af91e85ddfa752a315936a317ed32aa9970846c9
Phil <pblom@lanl.gov>
2019-10-18 11:02:00 -0600

    adding example atmos

--- examples/build_eofs.py
+++ examples/build_eofs.py
@@ -25,24 +25,23 @@ if __name__ == '__main__':
     #   Define parameters for   #
     #      EOF construction     #
     # ######################### #
-    loc_id = "label"
-    year_lims = [2010, 2016]
-    clustering_thresh = 0.2
-
+    loc_id = "example"
     prof_prefix = "g2stxt_"
 
+    clustering_thresh = 0.2
+    
     # ######################### #
     #  Build atmosphere matrix  #
     #   for EOF construction    #
     # ######################### #
-    if os.path.isfile(loc_id + "/eofs/" + loc_id + "-singular_values.dat"):
-        print(loc_id + "/eofs/" + loc_id + "-singular_values.dat exists" + '\n\t' + "Skipping construction of EOFs")
+    if os.path.isfile("eofs/" + loc_id + "-singular_values.dat"):
+        print("eofs/" + loc_id + "-singular_values.dat exists" + '\n\t' + "Skipping construction of EOFs")
     else:
         print("Building atmosphere matrix for all profiles...")
-        A0, z0 = eofs.build_atmo_matrix(loc_id + "/profs/{:02d}/".format(1), prof_prefix + "*")
+        A0, z0 = eofs.build_atmo_matrix("profs/{:02d}/".format(1), prof_prefix + "*")
         A_list = [A0]
         for n in range(1, 12):
-            A_temp, z_temp = eofs.build_atmo_matrix(loc_id + "/profs/{:02d}/".format(n + 1), prof_prefix + "*", ref_alts=z0)
+            A_temp, z_temp = eofs.build_atmo_matrix("profs/{:02d}/".format(n + 1), prof_prefix + "*", ref_alts=z0)
             if np.any(A_temp):
                 A_list += [A_temp]
     
@@ -51,45 +50,45 @@ if __name__ == '__main__':
             A = np.vstack((A, A_list[n]))
     
         print("Computing SVD...")
-        eofs.compute_svd(A, z0, loc_id + "/eofs/" + loc_id)
+        eofs.compute_svd(A, z0, "eofs/" + loc_id)
 
     # ################################### #
     #  Compute EOF coefficients for each  #
     #     month and build season stats    #
     # ################################### #
-    if os.path.isfile(loc_id + "/coeffs/" + loc_id + "_12-coeffs.npy"):
-        print(loc_id + "/coeffs/" + loc_id + "_12-coeffs.npy exists" + '\n\t' + "Skipping calcuation of EOF coefficients")
+    if os.path.isfile("coeffs/" + loc_id + "_12-coeffs.npy"):
+        print("coeffs/" + loc_id + "_12-coeffs.npy exists" + '\n\t' + "Skipping calcuation of EOF coefficients")
     else:
         print('\n' + "Computing EOF coefficients for each month...")
         pl = mp.ProcessingPool(int(cpu_count() - 1))
         for m in range(12):
-            Am, zm = eofs.build_atmo_matrix(loc_id + "/profs/{:02d}/".format(m + 1), prof_prefix + "*")
-            coeffs = eofs.compute_coeffs(Am, zm, loc_id + "/eofs/" + loc_id, loc_id + "/coeffs/" + loc_id + "_{:02d}".format(m + 1), eof_cnt=100, pool=pl)
+            Am, zm = eofs.build_atmo_matrix("profs/{:02d}/".format(m + 1), prof_prefix + "*")
+            coeffs = eofs.compute_coeffs(Am, zm, "eofs/" + loc_id, "coeffs/" + loc_id + "_{:02d}".format(m + 1), eof_cnt=100, pool=pl)
         pl.close()
         pl.terminate()
 
-    if os.path.isfile(loc_id + "/coeffs/" + loc_id + "-overlap.npy"):
-        print(loc_id + "/coeffs/" + loc_id + "-overlap.npy exists" + '\n\t' + "Skipping calculation of seasonality")
+    if os.path.isfile("coeffs/" + loc_id + "-overlap.npy"):
+        print("coeffs/" + loc_id + "-overlap.npy exists" + '\n\t' + "Skipping calculation of seasonality")
     else:
         coeffs = [0] * 12
         for m in range(12):
-            coeffs[m] = np.load(loc_id + "/coeffs/" + loc_id + "_{:02d}-coeffs.npy".format(m + 1))
+            coeffs[m] = np.load("coeffs/" + loc_id + "_{:02d}-coeffs.npy".format(m + 1))
 
         overlap = eofs.compute_overlap(coeffs)
-        np.save(loc_id + "/coeffs/" + loc_id + "-overlap", overlap)
+        np.save("coeffs/" + loc_id + "-overlap", overlap)
 
-    overlap = np.load(loc_id + "/coeffs/" + loc_id + "-overlap.npy")
-    eof_weight = np.loadtxt(loc_id + "/eofs/" + loc_id + "-singular_values.dat")[:, 1]
+    overlap = np.load("coeffs/" + loc_id + "-overlap.npy")
+    eof_weight = np.loadtxt("eofs/" + loc_id + "-singular_values.dat")[:, 1]
     overlap_proj = np.average(overlap, weights=eof_weight[:overlap.shape[0]], axis=0)
 
     dist_mat = -np.log(overlap_proj)
     np.fill_diagonal(dist_mat, 0.0)
-    hjl.cluster(dist_mat, clustering_thresh, show_result=True, file_id=loc_id + "/coeffs/" + loc_id + "-clustering")
+    hjl.cluster(dist_mat, clustering_thresh, show_result=True, file_id="coeffs/" + loc_id + "-clustering")
 
     M1, M2 = np.meshgrid(range(12), range(12))
     plt.figure(1, figsize=(4, 4))
     plt.scatter(M1.flatten() + 1, M2.flatten() + 1, c=overlap_proj.flatten(), cmap=cm.Blues, marker='s', s=200.0)
-    plt.savefig(loc_id + "/coeffs/" + loc_id + "-seasonality.png", dpi=300)
+    plt.savefig("coeffs/" + loc_id + "-seasonality.png", dpi=300)
 
 
 
--- examples/prep_eofs.py
+++ examples/prep_eofs.py
@@ -9,41 +9,42 @@ import os
 
 from stochprop import eofs
 
-# Set location and g2s file info
-loc_dir = "label"
+if __name__ == '__main__':
+    # ######################### #
+    #   Define parameters for   #
+    #      organizing data      #
+    # ######################### #
+    prof_dir = "dir/of/g2s/"
 
-prof_dir = "dir/of/g2s/"
-prof_prefix = "g2stxt_"
-year_lims = [2010, 2016]
-
-# build directories
-if not os.path.isdir(loc_dir + "/"):
-    print("Creating location directory...")
-    os.system("mkdir " + loc_dir)
-
-sub_dirs = ["coeffs", "eofs", "profs", "propagation", "samples"]
-for dir in sub_dirs:
-    if not os.path.isdir(loc_dir + "/" + dir):
-        print('\t', "Creating " + dir + " directory...")
-        os.system("mkdir " + loc_dir + "/" + dir)
-
-for M in range(1, 13):
-    if not os.path.isdir(loc_dir + "/profs/" + "{:02d}".format(M)):
-        os.system("mkdir " + loc_dir + "/profs/" + "{:02d}".format(M))
-
-seasons = ["fall", "spring", "summer", "winter"]
-for season in seasons:
-    if not os.path.isdir(loc_dir + "/samples/" + season):
-        os.system("mkdir " + loc_dir + "/samples/" + season)
-
-# move .dat profile files over
-print('\n' + "Moving profiles into profs directory...")
-for year in range(year_lims[0], year_lims[1] + 1):
-    print('\t' + "Moving " + "{:04d}".format(year) + " profiles...")
+    prof_prefix = "g2stxt_"
+    year_lims = [2010, 2016]
+    
+    # ######################### #
+    #    Build directories to   #
+    #      organize results     #
+    # ######################### #
+    sub_dirs = ["coeffs", "eofs", "profs", "propagation", "samples"]
+    seasons = ["fall", "spring", "summer", "winter"]
+    
+    print("Creating directories...")
+    for dir in sub_dirs:
+        if not os.path.isdir(dir):
+            os.system("mkdir " + dir)
+    
     for M in range(1, 13):
-        os.system("mv " + prof_dir + prof_prefix + "{:04d}".format(year) + "{:02d}".format(M) + "* " + loc_dir + "/profs/" + "{:02d}".format(M))
+        if not os.path.isdir("profs/" + "{:02d}".format(M)):
+            os.system("mkdir profs/" + "{:02d}".format(M))
 
-# Run QC
-print('')
-for M in range(1, 13):
-    eofs.profs_check(loc_dir + "/profs/{:02d}/".format(M), prof_prefix + "*")
+    for season in seasons:
+        if not os.path.isdir("samples/" + season):
+            os.system("mkdir samples/" + season)
+    
+    print('\n' + "Moving profiles into profs directory...")
+    for year in range(year_lims[0], year_lims[1] + 1):
+        print('\t' + "Moving " + "{:04d}".format(year) + " profiles...")
+        for M in range(1, 13):
+            os.system("mv " + prof_dir + prof_prefix + "{:04d}".format(year) + "{:02d}".format(M) + "* " + loc_dir + "/profs/" + "{:02d}".format(M))
+
+    # Run QC
+    for M in range(1, 13):
+        eofs.profs_check("profs/{:02d}/".format(M), prof_prefix + "*")
